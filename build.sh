dee577c219aa9447fe5b5873e0fa8d372c92dbb7
joweisberg <jo.weisberg@gmail.com>
2020-04-05 16:28:57 +0200

    Initial version

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd /home/media/docker-phpvirtualbox
-git clone https://github.com/jazzdd86/phpVirtualbox.git .
-sed -i "s/5.2-1/develop/g" Dockerfile
-# docker build -t jazzdd/phpvirtualbox .
-docker build -t local/phpvirtualbox .
